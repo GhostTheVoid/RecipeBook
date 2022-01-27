a5346af868f45d512d717f2aba606c06c37d879f
Ghost <67445574+GhostTheVoid@users.noreply.github.com>
2022-01-27 11:46:37 -0600

    2022-01-27

--- RecipeBook/src/recipebook/RecipeBook.java
+++ RecipeBook/src/recipebook/RecipeBook.java
@@ -1,17 +1,16 @@
 package recipebook;
 
-/** required imports */
-
 /** 
-* RecipeBook.java - 
+* RecipeBook.java - An application centered around storing Recipes to be 
+* able to return back to them in the future. Has a base level of Recipes 
+* from a group of locations, but also gives the user the option to 
+* create their own Recipes.
 *
 * @author Marissa Rowles
 * @teacher Mr. Wachs 
 * @since 20-Oct-2021 
 */
 public class RecipeBook {
-
-    // REUSABLE VARIABLES and METHODS (below this line)........................
     
     /**
      * Main method for the project
@@ -19,7 +18,6 @@ public class RecipeBook {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new WriteRecipes();
         new Globals();
     }
 }
